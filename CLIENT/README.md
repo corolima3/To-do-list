5f921f9ef59127066d89c3434029b64f14252d98
corolima3 <corolimajose@outlook.com>
2024-01-25 19:57:03 -0300

    notas de README

--- CLIENT/README.md
+++ CLIENT/README.md
@@ -1,4 +1,6 @@
 # React + Vite
 
 ## https://www.youtube.com/watch?v=NmkY4JgS21A&list=PLGTAwZ81WClcsLCq39KuYK288I_1FM4u4&index=2&t=8548s
-#### 3hs 01minuto 
+#### 3hs 01minuto
+
+git add https://github.com/
--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -6,6 +6,7 @@ import TasksPage from "./layout/TasksPage";
 import RegisterPage from "./layout/RegisterPage";
 import TasksFormPage from "./layout/TasksFormPage";
 import ProfilePage from "./layout/ProfilePage";
+import { ProtectedRoute } from './ProtectedRoute';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -19,9 +20,11 @@ function App() {
             <Route path='/' element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/tasks" element={<TasksPage />} />
-            <Route path="/add-tasks" element={<TasksFormPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route element={< ProtectedRoute />} >
+              <Route path="/tasks" element={<TasksPage />} />
+              <Route path="/add-tasks" element={<TasksFormPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
 
           </Routes>
         </BrowserRouter>
